4308b086fca3a232f93125c28d3fad8a155b4775
Gustavo Kath <gustavokath@outlook.com>
2018-02-24 17:57:07 -0300

    Fix issue in recast.ai integration json (#2)

--- recast-ai.js
+++ recast-ai.js
@@ -2,7 +2,7 @@ const quotesUtil = require('./quotes-util')
 
 module.exports = (app) => {
   app.post('/recast-ai/quote', (req, res) => {
-    var intent = getBestIntent(req.body.intents);
+    var intent = getBestIntent(req.body.nlp.intents);
     if(intent === 'new-quote'){
       res.send({
         replies: [{
@@ -17,6 +17,9 @@ module.exports = (app) => {
 }
 
 function getBestIntent(intentsList){
+  if(intentsList === undefined){
+    return undefined
+  }
   var intent = undefined;
   var confidence = 0;
   for(var i=0;i<intentsList.length;i++){
